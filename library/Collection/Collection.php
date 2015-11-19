9d2acb4d80b76fb4477bec650af5b39940287ece
Danny van Dam <d.van.dam@vnumedia.nl>
2015-11-19 21:40:37 +0100

    For now only use iterator

--- library/Collection/Collection.php
+++ library/Collection/Collection.php
@@ -5,12 +5,4 @@ namespace Collection;
 abstract class Collection implements \Iterator
 {
     use Iterator;
-
-    /**
-     * @param $function
-     * @return Map
-     */
-    public function map($function) {
-        return new static(array_map($function, $this->values));
-    }
 }
