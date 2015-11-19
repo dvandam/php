807b04bf5cd282c1fa703fa6ac8e57f946636002
Danny van Dam <d.van.dam@vnumedia.nl>
2015-11-19 21:42:04 +0100

    Create collection of words per number combination

--- php.php
+++ php.php
@@ -6,20 +6,46 @@ use Collection\String;
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-$words = new Set(file('http://lamp.epfl.ch/files/content/sites/lamp/files/teaching/progfun/linuxwords.txt'));
+$words = new Set(file('http://lamp.epfl.ch/files/content/sites/lamp/files/teaching/progfun/linuxwords.txt', FILE_IGNORE_NEW_LINES));
+//$words = new Set(["dit", "zijn", "wat", "woorden"]);
+$nummer = "34894569289667336";
 
 $nav = new Map([
-    '2' => new Set(new String("ABC")),
-    '3' => new Set(new String("DEF")),
-    '4' => new Set(new String("GHI")),
-    '5' => new Set(new String("JKL")),
-    '6' => new Set(new String("MNO")),
-    '7' => new Set(new String("PQRS")),
-    '8' => new Set(new String("TUV")),
-    '9' => new Set(new String("WXYZ"))
+    '2' => new String("ABC"),
+    '3' => new String("DEF"),
+    '4' => new String("GHI"),
+    '5' => new String("JKL"),
+    '6' => new String("MNO"),
+    '7' => new String("PQRS"),
+    '8' => new String("TUV"),
+    '9' => new String("WXYZ")
 ]);
 
 $numbers = new Map();
-foreach ($words as $word) {
-
+foreach ($nav as $number => $string) {
+    foreach ($string->orElse([]) as $character) {
+        $numbers->set((string)$character, $number);
+    }
 }
+
+$numberedWords = new Map();
+foreach ($words as $word) {
+    $key = '';
+    $word = $word->orElse('');
+    foreach (new String($word) as $char) {
+        $key .= $numbers->get(strtoupper($char->orElse('')))->orElse('0');
+    }
+    $wordsForKey = $numberedWords->get($key)->orElse(new Set());
+    $wordsForKey->add($word);
+    $numberedWords->set($key, $wordsForKey);
+}
+
+var_dump($numberedWords);
+
+//function getWords($left, $right = "") {
+//    $result = new Set();
+//    for ($i=1; $i<=strlen($left); $i++) {
+//        if () continue;
+//        foreach(getWords())
+//    }
+//}
