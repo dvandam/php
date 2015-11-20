512672a0aefcac6bb07343c334737de2734a03e2
Danny van Dam <d.van.dam@vnumedia.nl>
2015-11-20 15:38:30 +0100

    Map strings to other strings

--- library/Collection/String.php
+++ library/Collection/String.php
@@ -7,7 +7,7 @@ class String extends Collection
     /**
      * @param string $string
      */
-    public function __construct($string)
+    public function __construct($string = '')
     {
         foreach(str_split($string) as $character) parent::addValue($character);
     }
@@ -15,5 +15,23 @@ class String extends Collection
     public function __toString()
     {
         return join('', $this->getValues());
+    }
+
+    /**
+     * @param $string
+     * @return String
+     */
+    public function add($string)
+    {
+        return new self((string)$this . $string);
+    }
+
+    /**
+     * @param $function
+     * @return String
+     */
+    public function map($function)
+    {
+        return new self(join('', array_map($function, $this->getValues())));
     }
 }
